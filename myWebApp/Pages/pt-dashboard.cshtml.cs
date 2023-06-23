c871fd34ffb33f03cf1af1cd259d36b3bdd666ce
Jamie Homewood <JH9196@aru.ac.uk>
2023-06-23 11:06:50 +0100

    Added Student and Personal Tutor Dashboards.  Some known issues with buttons.  POC.

---
+++ myWebApp/Pages/pt-dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace myWebApp.Pages
+{
+    public class pt_dashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
